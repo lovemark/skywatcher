58c10e608e20f4907a466c6194303b1c3caba125
luis140219 <luis.camara@live.com.pt>
2014-08-30 08:41:19 +0100

    Create Program.cs
    SkyWatcher initializer

---
+++ Program.cs
@@ -0,0 +1,34 @@
+// SkyWatcher initialization
+
+using SkyWatcher.Properties;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SkyWatcher
+{
+    public static class Program
+    {
+        public static MainForm myForm;
+        public static void Main(string[] args)
+        {
+            CheckArguments(args);
+            SkyObjectLibrary.InitializeLibrary();
+            Console.SetIn(TextReader.Synchronized(new StreamReader(Console.OpenStandardInput(1024), Console.OutputEncoding, false, 1024, true)));
+            Console.WriteLine("SkyWatcher v1.0 with UI Complete (@ 2014-08-02 19:50) Luismark");
+            if (args.Length == 2)
+                myForm = new MainForm(int.Parse(args[0]), int.Parse(args[1]));
+            if (args.Length == 1)
+                myForm = new MainForm(args[0]);
+            if (args.Length == 0)
+                myForm = new MainForm(Settings1.Default.CurrentStar);
+            Application.Run(myForm);
+        }
+        public static void CheckArguments(string[] args) {
+            if (args.Length > 2) {
+                Console.WriteLine("Usage: SkyWatcher <[<ra> <dec>] | [<name>]>");
+                Environment.Exit(unchecked((int)(0x80070057)));
+            }
+        }
+    }
+}
