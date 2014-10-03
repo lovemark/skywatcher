88d489899bcb0333f1c8cff5d4daee423d61c727
Luís <luis.camara@live.com.pt>
2014-10-03 21:15:59 +0100

    Create PolarViewer.cs
    Taken the second and final step to close #13.

---
+++ PolarViewer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SkyWatcher {
+    public sealed class PolarViewer : Form {
+        bool isSouth;
+        public PolarViewer() {
+            Text = "SkyWatcher Polar Viewer";
+            Size = new Size(600, 600);
+            
+            MessageBox.Show("Click in the center of the Polar Viewer Window to switch between North and South poles.");
+            
+            VisibleChanged += delegate(object sender, EventArgs e) {
+                if (Visible) {
+                    MakeStars();
+                }
+            };
+            MouseClick += delegate(object sender, MouseEventArgs e) {
+                if (e.X >= 290 && e.Y >= 290 && e.X < 310 && e.Y < 310) {
+                    isSouth = !isSouth;
+                    MakeStars();
+                }
+            }
+        }
+        public void MakeStars() {
+            const double TAU = 2 * Math.PI;
+            for (int i = Controls.Count - 1; i >= 0; i++) {
+                Controls.RemoveAt(i);
+            }
+            for (int j = 0; j < Star.totalstars; j++) {
+                try {
+                    Star result = SkyObjectLibrary.GetItem(j);
+                    if (Math.Abs(j.Dec) < 50 || (isSouth ^ (j.Dec / Math.Abs(j.Dec) == 1))) {
+                        if (isSouth) {
+                            double x = -300 * Math.Cos(0.261799387799149 * j.RA) + 300;
+                            double y = 300 * Math.Sin(0.261799387799149 * j.RA) + 300;
+                        } else {
+                            double x = -300 * Math.Cos(TAU - (0.261799387799149 * j.RA)) + 300;
+                            double y = 300 * Math.Sin(TAU - (0.261799387799149 * j.RA)) + 300;
+                        }
+                    }
+                } catch {
+                    continue;
+                }
+            }
+        }
+        
+    }
+}
