9e67ea7e43c6793204b24df2420f3bb8cbeb0230
Luís Câmara <luis.camara@live.com.pt>
2014-10-08 09:00:33 +0100

    Removed Settings references

--- Program.cs
+++ Program.cs
@@ -15,13 +15,13 @@ namespace SkyWatcher
             CheckArguments(args);
             SkyObjectLibrary.InitializeLibrary();
             Console.SetIn(TextReader.Synchronized(new StreamReader(Console.OpenStandardInput(1024), Console.OutputEncoding, false, 1024, true)));
-            Console.WriteLine("SkyWatcher v1.0 with UI Complete (@ 2014-08-02 19:50) Luismark");
+            Console.WriteLine("SkyWatcher v1.0 with UI Complete (@ 2014-10-08 19:50) Luismark");
             if (args.Length == 2)
                 myForm = new MainForm(int.Parse(args[0]), int.Parse(args[1]));
             if (args.Length == 1)
                 myForm = new MainForm(args[0]);
             if (args.Length == 0)
-                myForm = new MainForm(Settings1.Default.CurrentStar);
+                myForm = new MainForm((Math.Abs(DateTime.Now.Month - 7) <= 2) ? "Altair" : "Sirius");
             Application.Run(myForm);
         }
         public static void CheckArguments(string[] args) {
