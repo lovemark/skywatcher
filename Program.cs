32485227dc983f08d2802ee74a08851a1d3e6738
Bárbara Câmara <barbara.camara@live.com.pt>
2014-10-27 21:24:38 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 // SkyWatcher initialisation
 
-using SkyWatcher.Properties;
 using System;
 using System.IO;
 using System.Windows.Forms;
@@ -15,13 +14,13 @@ namespace SkyWatcher
             CheckArguments(args);
             SkyObjectLibrary.InitialiseLibrary();
             Console.SetIn(TextReader.Synchronized(new StreamReader(Console.OpenStandardInput(1024), Console.OutputEncoding, false, 1024, true)));
-            Console.WriteLine("SkyWatcher v1.0 with UI Complete (@ 2014-10-22 22:05) Luismark");
+            Console.WriteLine("SkyWatcher v1.0 with UI Complete (@ 2014-10-08 19:50) Luismark");
             if (args.Length == 2)
                 myForm = new MainForm(int.Parse(args[0]), int.Parse(args[1]));
             if (args.Length == 1)
                 myForm = new MainForm(args[0]);
             if (args.Length == 0)
-                myForm = new MainForm(Settings1.Default.CurrentStar);
+                myForm = new MainForm((Math.Abs(DateTime.Now.Month - 7) <= 2) ? "Altair" : "Sirius");
             Application.Run(myForm);
         }
         public static void CheckArguments(string[] args) {
