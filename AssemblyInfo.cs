d887223bf416bb8a2a4bc70d31108324cbab1e15
Luís Câmara <luis.camara@live.com.pt>
2017-11-01 13:32:47 +0000

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SkyWatcher")]
+[assembly: AssemblyDescription("A program for Windows that is a sky guide.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Luismark")]
+[assembly: AssemblyCopyright("Copyright © 2014-2017 Luismark. Licenced under the GNU General Public License (https://www.gnu.org/licenses/gpl.html) version 2 or any later version.")]
+[assembly: AssemblyProduct("SkyWatcher")]
+[assembly: AssemblyTrademark("Luismark")]
+[assembly: AssemblyCulture("")]
+
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible(false)]
+[assembly: Guid("3A79A037-20E3-42E1-9209-75784288AB71")]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+[assembly: AssemblyVersion("1.0.21.183")]
+
